5dae5817c53a7e1e84069a8e6b71a0a989540ddd
Adekola Olawale <rolawale92@gmail.com>
2023-09-20 17:48:39 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import ProductDetails from './pages/ProductDetails';
+import Cart from './pages/Cart';
+import Header from './components/Header';
+import './assets/styles/App.css';
+import UserProfile from './pages/UserProfile';
+import ProtectedRoute from './components/ProtectedRoute';
+import { Login } from './pages/Login';
+import SignUp from './pages/SignUp';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	useEffect(() => {});
+
+	return (
+		<Router>
+			<div className='app'>
+				{/* Navigation */}
+				<Header />
+
+				<Routes>
+					<Route exact path='/' element={<Home />} />
+					<Route path='/product/:id' element={<ProductDetails />} />
+					<Route path='/cart' element={<Cart />} />
+					<Route path='/login' element={<Login />} />
+					<Route path='/signup' element={<SignUp />} />
+					<Route
+						path='/profile'
+						element={<ProtectedRoute element={<UserProfile />} />}
+					/>
+				</Routes>
+			</div>
+		</Router>
+	);
+};
 
 export default App;
